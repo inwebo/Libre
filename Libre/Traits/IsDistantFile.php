75a8ba4ddcdf1d5930a61683f36dbabbecac339f
inwebo <inwebo@gmail.com>
2015-07-27 09:04:27 +0200

    Add trais

--- Libre/Traits/IsDistantFile.php
+++ Libre/Traits/IsDistantFile.php
@@ -1,15 +1,27 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: inwebo
- * Date: 26/07/15
- * Time: 18:41
- */
+namespace Libre\Traits {
 
-namespace Libre\Traits;
+    trait isDistantFile {
 
+        public function isDistantFile( $filename ) {
 
-class IsDistantFile
-{
+            if( filter_var( $filename, FILTER_VALIDATE_URL ) ) {
+
+                $opts = array(
+                    'http' => array(
+                        'method' => 'HEAD'
+                    )
+                );
+
+                $context = stream_context_create( $opts );
+
+                $file = file_get_contents( $filename, false, $context, null, 0 );
+
+                return ( $file !== false ) ? true : false;
+            }
+            return false;
+        }
+
+    }
 
 }
