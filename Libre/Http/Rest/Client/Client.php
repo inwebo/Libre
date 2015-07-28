76d402fcf709638fd003ce108d21b4e8315742e4
Inwebo <github@inwebo.net>
2015-07-28 11:00:22 +0200

    Step
    Ajout de l'auth basic
    Ajout de callbacksvalidator
    Ajout de setHeaders

--- Libre/Http/Rest/Client/Client.php
+++ Libre/Http/Rest/Client/Client.php
@@ -55,6 +55,7 @@ namespace Libre\Http\Rest {
 
     class Client {
 
+        //region Attributs
         /**
          * @var string MUST END WITH FINAL /
          */
@@ -64,6 +65,69 @@ namespace Libre\Http\Rest {
          * @var array
          */
         protected $_defaultStreamOptions;
+
+        /**
+         * @var array
+         */
+        protected $_headers = array();
+
+        /**
+         * @var array
+         */
+        protected $_callbackValidators = array();
+        //endregion Attributs
+
+        //region Getters / Setters
+        /**
+         * @return array
+         */
+        public function getDefaultStreamOptions()
+        {
+            return $this->_defaultStreamOptions;
+        }
+
+        /**
+         * @param array $defaultStreamOptions
+         */
+        public function setDefaultStreamOptions($defaultStreamOptions)
+        {
+            $this->_defaultStreamOptions = $defaultStreamOptions;
+        }
+
+        public function getHeadersAsString()
+        {
+            $buffer = '';
+            foreach($this->getHeaders() as $key => $value)
+            {
+                $buffer .= trim($key, ':') . ':' . ' '.trim($value, ' '). "\r\n";
+            }
+            return $buffer;
+        }
+
+        /**
+         * @return array
+         */
+        public function getHeaders()
+        {
+            return $this->_headers;
+        }
+
+        /**
+         * @param string $name|array
+         * @param string $value
+         * @param bool $erase
+         */
+        public function setHeaders($name, $value, $erase = false)
+        {
+            if( is_array($name) )
+            {
+                $this->_headers = array_merge($this->_headers, $name);
+            }
+            elseif((isset($this->_headers[$name]) && $erase) || !isset($this->_headers[$name]))
+            {
+                $this->_headers[$name] = $value;
+            }
+        }
 
         /**
          * @return string
@@ -80,6 +144,20 @@ namespace Libre\Http\Rest {
         {
             $this->_serviceUrl = $serviceUrl;
         }
+
+        /**
+         * @return array
+         */
+        public function getCallbackValidators()
+        {
+            return $this->_callbackValidators;
+        }
+
+        public function setCallbackValidators(\Closure $callbackValidators)
+        {
+            $this->_callbackValidators[] = $callbackValidators;
+        }
+        //endregion Getters / Setters
 
         //region URL
         public function parseUrl()
@@ -110,8 +188,9 @@ namespace Libre\Http\Rest {
          * @param string MUST END WITH FINAL /
          * @throws RestClientException Si url invalid
          * @throws RestClientException Si le status code de l'en tête HTTP n'est pas égale à 300, rfc2616.
-         * @see http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
-         * @see https://developer.att.com/application-resource-optimizer/docs/best-practices/http-300-status-codes
+         * @link http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
+         * @link https://developer.att.com/application-resource-optimizer/docs/best-practices/http-300-status-codes
+         * @todo Digest auth
          */
         public function __construct($serviceUrl)
         {
@@ -120,13 +199,14 @@ namespace Libre\Http\Rest {
             {
                 $this->setServiceUrl($serviceUrl);
                 $this->isValidService();
+                $this->setHeaders('Content-type', 'application/x-www-form-urlencoded');
             }
         }
 
         /**
-         * @todo Le status code de l'en tête HTML de réponse sur l'url <code>$_serviceUrl</code> DOIT renvoyer 300,
          * pour être une source valide.
          * @return bool
+         * @todo Le status code de la réponse devrait renvoyé le code 300.
          */
         public function isValidService()
         {
@@ -139,9 +219,10 @@ namespace Libre\Http\Rest {
          */
         protected function getDefaultStreamContext($verb, $header=array(),$content=array())
         {
+            $this->setHeaders($header);
             $options = array(
                 $this->getProtocol() => array(
-                    'header'    => 'Content-type: application/x-www-form-urlencoded',
+                    'header'    => $this->getHeadersAsString(),
                     'method'    => strtoupper($verb),
                     'content'   => http_build_query($content)
                 )
@@ -154,7 +235,7 @@ namespace Libre\Http\Rest {
          * @param $verb
          * @param string $uri
          * @param null $options
-         * @return \Libre\Http\Rest\Response
+         * @return Response
          * @throws RestClientException
          * @throws RestResponseException
          */
@@ -177,300 +258,83 @@ namespace Libre\Http\Rest {
             }
         }
 
+        /**
+         * @param Response $response
+         * @return bool
+         */
+        public function callCallbacksValidator(Response $response)
+        {
+            $valid = true;
+            if( !empty($this->getCallbackValidators()) )
+            {
+                foreach($this->getCallbackValidators() as $closure)
+                {
+                    $valid &= $closure->__invoke($response);
+                }
+            }
+            return (bool)$valid;
+        }
+
+        //region Auth
+        public function authBasic($base64String)
+        {
+            $this->setHeaders('Authorization', 'Basic ' . $base64String);
+        }
+        public function authBasicLoginPwd($user, $password)
+        {
+            $this->setHeaders('Authorization', 'Basic ' . base64_encode($user . ':' . $password));
+        }
+        /**
+         * @todo
+         */
+        public function authDigest()
+        {
+
+        }
+        /**
+         * @todo
+         */
+        public function authDigestLoginPwd()
+        {
+
+        }
+        //endregion Auth
+
         //region VERBS
         /**
          * @param string $uri
-         * @return \Libre\Http\Rest\Response
+         * @return Response
          * @throws RestClientException
          * @throws RestResponseException
          */
         public function get($uri='')
         {
-            return $this->open('GET',$uri);
+            return $this->open('GET', $uri);
         }
         public function post($uri='', $params)
         {
-            return $this->open('POST',$uri, $params);
+            return $this->open('POST', $uri, $params);
         }
         public function update($uri='', $params)
         {
-            return $this->open('UPDATE',$uri, $params);
+            return $this->open('UPDATE', $uri, $params);
         }
         public function delete($uri='', $params)
         {
-            return $this->open('DELETE',$uri, $params);
+            return $this->open('DELETE', $uri, $params);
         }
         public function head($uri='')
         {
-            return $this->open('HEAD',$uri);
+            return $this->open('HEAD', $uri);
         }
         public function options($uri='')
         {
-            return $this->open('OPTIONS',$uri);
+            return $this->open('OPTIONS', $uri);
         }
         public function patch($uri='')
         {
-            return $this->open('PATCH',$uri);
+            return $this->open('PATCH', $uri);
         }
-        //@todo ajouter les autres verb possible
-        //endregion CRUD
-
-    }
-
-    class _Client {
-
-        /**
-         * @var string Url de l'hôte qui hébérge le service REST. Fonctionne pour les protocoles http et https.
-         */
-        private $host;
-
-        /**
-         * @var string L'url du service..
-         */
-        private $url;
-
-        /**
-         * @var string URI compléte du service.
-         */
-        private $uri;
-
-        /**
-         * @var bool Le Service REST nécessite t-il une autentification.
-         */
-        private $logged = false;
-
-        /**
-         * @var string $user L'utilisateur courant.
-         */
-        private $user;
-
-        /**
-         * @var string Signature unique de l'utilisateur courant.
-         */
-        private $signature;
-
-        /**
-         * @var bool Le server REST est il https.
-         */
-        public $isHttps;
-
-        public function __construct( $host, $url = "" ) {
-            $this->host = $host;
-            $this->url = $url;
-            $this->uri = $this->getUri();
-            $this->isHttps = $this->isHttps();
-        }
-
-        /**
-         * Requête http HEAD. Ne retourne que l'en tête HTTP de la requête courante.
-         *
-         * @param array $params Paramètres aditionnels à envoyer avec la requête.
-         * @return bool|\StdClass False si l'url n'est pas valide, sinon un objet.
-         */
-        protected function head( $params = array() ) {
-            return $this->exec( $this->context("HEAD", $params) );
-        }
-
-        /**
-         * Requête http POST. Permet la création de nouveaux items sur le serveur REST.
-         *
-         * @param array $params Paramètres aditionnels à envoyer avec la requête.
-         * @return bool|\StdClass False si l'url n'est pas valide, sinon un objet.
-         */
-        public function create( $params = array() ) {
-            return  $this->exec( $this->context("POST", $params),$params );
-        }
-
-        /**
-         * Requête http GET. Permet la lecture d'elements.
-         *
-         * @param array $params Paramètres aditionnels à envoyer avec la requête.
-         * @return bool|\StdClass False si l'url n'est pas valide, sinon un objet.
-         */
-        public function read( $params = array() ) {
-            return $this->exec( $this->context("GET " . $this->url, $params) );
-        }
-
-        /**
-         * Requête http PUT. Permet la mise à jours d'éléments.
-         *
-         * @param array $params Paramètres aditionnels à envoyer avec la requête.
-         * @return bool|\StdClass False si l'url n'est pas valide, sinon un objet.
-         */
-        public function update( $params = array() ) {
-            return $this->exec( $this->context("PUT", $params) );
-        }
-
-        /**
-         * Requête http DELETE. Permet la suppression d'éléments.
-         *
-         * @param array $params Paramètres aditionnels à envoyer avec la requête.
-         * @return bool|\StdClass False si l'url n'est pas valide, sinon un objet.
-         */
-        public function delete( $params = array() ) {
-            return $this->exec( $this->context("DELETE", $params) );
-        }
-
-        /**
-         * Le serveur est il https ?
-         *
-         * @return bool <code>True</code> si le serveur est https sinon <code>False</code>.
-         */
-        protected function isHttps() {
-            $url = parse_url($this->uri);
-            return ( !strstr($url['scheme'], "https") ) ? false : true ;
-        }
-
-        /**
-         * Construit l'uri. Concaténation du serveur et de l'url.
-         *
-         * @return string L'uri courante
-         */
-        protected function getUri() {
-            return $this->host . $this->url;
-        }
-
-        /**
-         * Création d'un nouveau client REST.
-         *
-         * @param string $host L'hôte à questionner.
-         * @param string $url L'url à questionner.
-         * @return Client Une instance d'un client,
-         */
-        static public function query( $host, $url = "" ) {
-            return new Client( $host, $url );
-        }
-
-        /**
-         * Setter host
-         *
-         * @param string $host Nouvel hote
-         * @return object $this Instance client courante.
-         */
-        public function host($host) {
-            $this->host = $host;
-            $this->uri = $this->getUri();
-            return $this;
-        }
-
-        /**
-         * Setter url
-         *
-         * @param string $url Url à questionner
-         * @return object $this Instance client courante.
-         */
-        public function url($url) {
-            $this->url = $url;
-            $this->uri = $this->getUri();
-            return $this;
-        }
-
-        /**
-         * Test si une uri est valide. C'est à dire si la réponse HTTP contient le code 200.
-         *
-         * @return bool Vrai si l'uri renvoit un code HTTP 200 sinon false.
-         */
-        public function isValidUrl() {
-            stream_context_set_default(
-                array(
-                    'http' => array(
-                        'method' => 'HEAD'
-                    )
-                )
-            );
-            @get_headers($this->uri);
-            $response_code = http_response_code();
-
-            return ( $response_code !== 200 ) ? false : true ;
-        }
-
-        /**
-         * Signe l'en tête d'une requête HTTP pour permettre une reqête REST nécessitant une autentification.
-         *
-         * @param String $user
-         * @param String $passPhrase
-         * @return Object $this
-         */
-        public function login( $user, $passPhrase ) {
-            $this->user = $user;
-            $this->logged = true;
-            $this->signature = self::signature( $user, $passPhrase, now() );
-            return $this;
-        }
-
-        /**
-         * Logout de l'utilisateur courant
-         *
-         * @return object $this
-         */
-        public function logout() {
-            $this->logged = false;
-            $this->signature = null;
-            return $this;
-        }
-
-        /**
-         * Création du context de flux de la requête courante.
-         *
-         * @param String $method La méthode HTTP souhaitée.
-         * @param array $params Les paramètres à envoyés avec la requête.
-         * @return resource
-         * @todo Accept : text/plain text/json  etc
-         * @todo Autorization  https://en.wikipedia.org/wiki/List_of_HTTP_header_fields
-         */
-        protected function context( $method, $params = array() ) {
-            $streamOptions = array(
-                'http'=>array(
-                    'method'=>$method,
-                    'header'=>'Content-type: application/x-www-form-urlencode',
-                    'host'=>$this->host
-                )
-            );
-
-            if( $this->logged ) {
-                $params = array_merge($params, array( "token"=> $this->signature,"user"=>$this->user, "timestamp"=> time() ) );
-            }
-
-            if( is_array($params) && !empty($params) ) {
-                $params = json_encode( $params);
-                $streamOptions['http']['content'] = $params;
-            }
-            return stream_context_create($streamOptions);
-        }
-
-        /**
-         * Envoit une requête vers l'uri courante avec comme context de flux $context.
-         *
-         * @param resource $context Le contexte de la requête à executer.
-         * @return bool|\StdClass
-         */
-        public function exec( $context ) {
-            $answer = new \StdClass();
-            if ( ( $stream = fopen( $this->uri, 'r', false, $context ) ) !== false){
-                $content = stream_get_contents($stream);
-                $header = stream_get_meta_data($stream);
-                fclose($stream);
-                $answer->header = $header;
-                $answer->content = $content;
-                return $answer;
-            }
-            else {
-                return false;
-            }
-        }
-
-        /**
-         * Permet de signer une requête HTTP.
-         *
-         * @param string $user L'utilisateur courant
-         * @param string $passPhrase La pass phrase associée à l'utilisateur courant
-         * @param int $timestamp
-         * @return string
-         */
-        static public function signature( $user, $passPhrase, $timestamp ) {
-            // @todo le md5 devrait provenir de la bdd
-            return  base64_encode( hash_hmac( "sha256", $user , $passPhrase . $timestamp ) ) ;
-        }
-
+        //endregion VERBS
     }
 }
