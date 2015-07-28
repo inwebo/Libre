f966d4da53c463553d20349d3411e0f28250cf1a
Inwebo <github@inwebo.net>
2015-07-28 11:01:10 +0200

    Step
    Ajout du retour des header sous forme de tableau

--- Libre/Http/Rest/Response/Response.php
+++ Libre/Http/Rest/Response/Response.php
@@ -5,7 +5,7 @@ namespace Libre\Http\Rest {
     class RestResponseException extends \Exception{}
 
     class Response
-    {
+        {
         /**
          * @var resource
          */
@@ -28,18 +28,21 @@ namespace Libre\Http\Rest {
         }
 
         /**
-         * @see https://secure.php.net/manual/en/function.stream-get-meta-data.php
+         * @link https://secure.php.net/manual/en/function.stream-get-meta-data.php
          * @return array
          */
         public function getMetaData()
         {
-            return stream_get_meta_data($this->getResource());
+            $array = stream_get_meta_data($this->getResource());
+            $buffer = $this->parse_http_response(implode("\r\n", $array['wrapper_data']));
+            $array['headers'] = $buffer[0];
+            return $array;
         }
 
         /**
          * @param null|int $maxLength
          * @param null|int $offset
-         * @see https://secure.php.net/manual/en/function.stream-get-contents.php
+         * @link https://secure.php.net/manual/en/function.stream-get-contents.php
          * @return string
          */
         public function getContent($maxLength = -1, $offset = null)
@@ -53,14 +56,48 @@ namespace Libre\Http\Rest {
          */
         public function __construct($resource)
         {
-            if( is_resource($resource) )
-            {
+            if (is_resource($resource)) {
                 $this->setResource($resource);
-            }
-            else
-            {
+            } else {
                 throw new RestClientException('Is not a resource');
             }
+        }
+
+
+        /**
+         * @param $string
+         * @return array
+         * @link http://snipplr.com/view/17242/parse-http-response/
+         */
+        protected function parse_http_response($string)
+        {
+            $headers = array();
+            $content = '';
+            $str = strtok($string, "\n");
+            $h = null;
+            $code = explode(' ', $str);
+            $headers['code'] = (int)$code[1];
+            while ($str !== false) {
+                if ($h and trim($str) === '') {
+                    $h = false;
+                    continue;
+                }
+                if ($h !== false and false !== strpos($str, ':')) {
+                    $h = true;
+                    list($headername, $headervalue) = explode(':', trim($str), 2);
+                    $headername = strtolower($headername);
+                    $headervalue = ltrim($headervalue);
+                    if (isset($headers[$headername]))
+                        $headers[$headername] .= ',' . $headervalue;
+                    else
+                        $headers[$headername] = $headervalue;
+                }
+                if ($h === false) {
+                    $content .= $str . "\n";
+                }
+                $str = strtok("\n");
+            }
+            return array($headers, trim($content));
         }
 
         public function close()
