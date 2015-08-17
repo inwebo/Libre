a22ab10cfec63712b7aaf29b5ff6d6dee87e54b9
inwebo <inwebo@gmail.com>
2015-08-17 20:29:40 +0200

    Step

--- Libre/System/Boot/BootStrap/Mvc/Task/FrontController/FrontController.php
+++ Libre/System/Boot/BootStrap/Mvc/Task/FrontController/FrontController.php
@@ -4,16 +4,14 @@ namespace Libre\System\Boot\BootStrap\Mvc\Task;
 
 use Libre\System\Boot\BootStrap\Mvc\DefaultTask;
 use Libre\Mvc\FrontController as Fc;
-use Libre\Mvc\FrontController\Decorator as MetaRouted;
 
 class FrontController extends DefaultTask
 {
     protected function invoker()
     {
-        $routed = $this->getSystem()->getRouted();
-        $fc     = new Fc($routed);
-        $fc->pushDecorator(new MetaRouted($routed->getDispatchable(), $routed->getAction(), $routed->getParams()));
-        $response = $fc->invoker();
+        $routed     = $this->getSystem()->getRouted();
+        $fc         = new Fc($routed);
+        $response   = $fc->invoker();
         $this->getSystem()->setResponse($response);
     }
 }
