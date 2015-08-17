b766218e5c77796a8ad6ad21e5f11197d64aa4de
inwebo <inwebo@gmail.com>
2015-08-17 19:55:17 +0200

    Add Router & FrontController task

--- Libre/System/Boot/BootStrap/Mvc/Mvc.php
+++ Libre/System/Boot/BootStrap/Mvc/Mvc.php
@@ -7,6 +7,9 @@ use Libre\System\Boot\AbstractTasks;
 use Libre\System\Boot\BootStrap\Mvc\Task\Init;
 use Libre\System\Boot\BootStrap\Mvc\Task\Modules;
 use Libre\System\Boot\BootStrap\Mvc\Task\Themes;
+use Libre\System\Boot\BootStrap\Mvc\Task\Router;
+use Libre\System\Boot\BootStrap\Mvc\Task\FrontController;
+
 class Mvc extends AbstractTasks
 {
     public function __construct(System $system, Config $config)
@@ -15,5 +18,7 @@ class Mvc extends AbstractTasks
         $this->attach(new Init($system, $config));
         $this->attach(new Modules($system, $config));
         $this->attach(new Themes($system, $config));
+        $this->attach(new Router($system, $config));
+        $this->attach(new FrontController($system, $config));
     }
 }
