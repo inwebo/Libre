09aa8115e85f74cca798150dee9f8ad303b4c33b
inwebo <inwebo@gmail.com>
2015-08-17 19:54:56 +0200

    Step

--- Libre/Models/Theme/Theme.php
+++ Libre/Models/Theme/Theme.php
@@ -3,15 +3,53 @@ namespace Libre\Models;
 
 class Theme extends Module
 {
-    public function getBaseJsDir()
+
+    protected function getter($section, $type)
     {
         $buffer = array();
-        $assets = $this->getConfig()->getSection('Theme');
+        $assets = $this->getConfig()->getSection($section);
 
         foreach($assets as $k=>$v)
         {
-            $buffer[$this->getPathsLocator()->getJsDir() . $k] = null;
+
+            if($v === $type)
+            {
+                $buffer[$this->getPathsLocator()->getCssUrl() . $k] = null;
+            }
+            elseif($v === $type){
+                $buffer[$this->getPathsLocator()->getJsUrl() . $k] = null;
+            }
         }
         return $buffer;
+    }
+
+    public function getBaseJsUrl()
+    {
+        return $this->getter('Base', 'js');
+    }
+
+    public function getLocalJsUrl()
+    {
+        return $this->getter('Local', 'js');
+    }
+
+    public function getJs()
+    {
+        return array_merge(array_keys($this->getBaseJsUrl()), array_keys($this->getLocalJsUrl()));
+    }
+
+    public function getBaseCssUrl()
+    {
+        return $this->getter('Base', 'css');
+    }
+
+    public function getLocalCssUrl()
+    {
+        return $this->getter('Local', 'css');
+    }
+
+    public function getCss()
+    {
+        return array_merge(array_keys($this->getBaseCssUrl()), array_keys($this->getLocalCssUrl()));
     }
 }
