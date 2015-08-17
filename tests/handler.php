ab382d2ddba0f5aa17e108aefd0573336d283c60
inwebo <inwebo@gmail.com>
2015-08-17 19:56:45 +0200

    Typo

--- tests/handler.php
+++ tests/handler.php
@@ -4,9 +4,7 @@ namespace Libre\Autoloader\tests\units {
     require_once 'atoum.phar';
     require_once __DIR__ .'/../Libre/index.php';
 
-    use Libre\Autoloader\tests\units\ClassInfos;
     use Libre\Session;
-    use Libre\Task;
     use mageekguy\atoum;
 
     class Handler extends atoum\test {
