f3032a80e7706c261187e927b4c733b41e803bdc
Julien <j.hannotin@aims-interactive.com>
2015-08-21 17:56:21 +0200

    Step

--- demos/assets/instances/www.test.fr/index.php
+++ demos/assets/instances/www.test.fr/index.php
@@ -1,1 +1,10 @@
 <h1>layout : index.php</h1>
+<p>
+    <?php $this->renderPartial('body'); ?>
+</p>
+<p>
+    <?php var_dump($this->foo);?>
+</p>
+<p>
+    <?php var_dump(\Libre\Session::this());?>
+</p>
