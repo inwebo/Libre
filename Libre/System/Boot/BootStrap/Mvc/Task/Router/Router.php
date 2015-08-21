b9a38b133657af21811cd76cd63ed0aa0a4b266a
Julien <j.hannotin@aims-interactive.com>
2015-08-21 17:55:24 +0200

    Step

--- Libre/System/Boot/BootStrap/Mvc/Task/Router/Router.php
+++ Libre/System/Boot/BootStrap/Mvc/Task/Router/Router.php
@@ -1,29 +1,29 @@
 <?php
 
-namespace Libre\System\Boot\BootStrap\Mvc\Task;
+namespace Libre\System\Boot\BootStrap\Mvc\Task {
 
-use Libre\Routing\Routed;
-use Libre\Routing\Uri;
-use Libre\System\Boot\BootStrap\Mvc\DefaultTask;
-use Libre\Routing\Router as _Router;
+    use Libre\Routing\Routed;
+    use Libre\Routing\Uri;
+    use Libre\System\Boot\BootStrap\Mvc\DefaultTask;
+    use Libre\Routing\Router as _Router;
 
-class Router extends DefaultTask
-{
-
-    protected function router()
+    class Router extends DefaultTask
     {
-        try
-        {
-            $router = new _Router(Uri::this(),$this->getSystem()->getRoutesCollection());
-            $routed = $router->dispatch();
-            //var_dump($routed);
-            //$tmpRouted = new Routed('Test','index');
-            //$this->getSystem()->setRouted($tmpRouted);
-        }
-        catch(\Exception $e)
-        {
-            // Default route
 
+        protected function router()
+        {
+            try
+            {
+                $router = new _Router(Uri::this(),$this->getSystem()->getRoutesCollection());
+                $routed = $router->dispatch();
+                // @todo routed !
+                //$this->getSystem()->setRouted($tmpRouted);
+            }
+            catch(\Exception $e)
+            {
+                // @todo Default route
+
+            }
         }
     }
 }
