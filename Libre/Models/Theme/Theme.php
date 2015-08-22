77df8d81bd7398b720828b5a4de48436acfadbad
inwebo <inwebo@gmail.com>
2015-08-22 22:09:18 +0200

    Fixe paths

--- Libre/Models/Theme/Theme.php
+++ Libre/Models/Theme/Theme.php
@@ -1,55 +1,123 @@
 <?php
 namespace Libre\Models;
 
+use Libre\System;
+use Libre\Files\Config;
+use Libre\System\Services\PathsLocator;
+
 class Theme extends Module
 {
+    /**
+     * @var System
+     */
+    protected $_system;
 
-    protected function getter($section, $type)
+    /**
+     * @return System
+     */
+    public function getSystem()
     {
-        $buffer = array();
-        $assets = $this->getConfig()->getSection($section);
+        return $this->_system;
+    }
 
-        foreach($assets as $k=>$v)
+    /**
+     * @param System $system
+     */
+    public function setSystem($system)
+    {
+        $this->_system = $system;
+    }
+
+    public function __construct(Config $config, PathsLocator $pathsLocator,System $system)
+    {
+        $this->setConfig($config);
+        $this->setPathsLocator($pathsLocator);
+        $this->setSystem($system);
+    }
+
+    /**
+     * @param string $type css | js
+     * @return string
+     */
+    protected function getPrefixByType($type)
+    {
+        return ($type==='css') ? 'css/' : 'js/' ;
+    }
+
+    public function getBaseAssets()
+    {
+        return $this->getConfig()->getSection('Base');
+    }
+    public function getLocalAssets()
+    {
+        return $this->getConfig()->getSection('Local');
+    }
+
+    public function getBasePublicUrl($type = 'css')
+    {
+        return $this->getSystem()->getInstanceLocator()->getPublicUrl() . $this->getPrefixByType($type);
+    }
+    public function getLocalPublicUrl($type = 'css')
+    {
+        if($type==='css')
         {
-
-            if($v === $type)
-            {
-                $buffer[$this->getPathsLocator()->getCssUrl() . $k] = null;
-            }
-            elseif($v === $type){
-                $buffer[$this->getPathsLocator()->getJsUrl() . $k] = null;
-            }
+            return $this->getPathsLocator()->getCssUrl();
         }
-        return $buffer;
-    }
-
-    public function getBaseJsUrl()
-    {
-        return $this->getter('Base', 'js');
-    }
-
-    public function getLocalJsUrl()
-    {
-        return $this->getter('Local', 'js');
+        if($type==='js')
+        {
+            return $this->getPathsLocator()->getJsUrl();
+        }
     }
 
     public function getJs()
     {
-        return array_merge(array_keys($this->getBaseJsUrl()), array_keys($this->getLocalJsUrl()));
-    }
+        $buffer = [];
+        $baseJs = $this->getBasePublicUrl('js');
+        $localJs = $this->getLocalPublicUrl('js');
 
-    public function getBaseCssUrl()
-    {
-        return $this->getter('Base', 'css');
-    }
+        $baseAssets = $this->getBaseAssets();
+        foreach($baseAssets as $k=>$v)
+        {
+            if( $v === 'js' )
+            {
+                $buffer[ $baseJs . $k ]= $v;
+            }
+        }
+        $localAssets = $this->getLocalAssets();
+        foreach($localAssets as $k=>$v)
+        {
+            if( $v === 'js' )
+            {
+                $buffer[ $localJs . $k ]= $v;
+            }
+        }
 
-    public function getLocalCssUrl()
-    {
-        return $this->getter('Local', 'css');
+        return $buffer;
     }
-
     public function getCss()
     {
-        return array_merge(array_keys($this->getBaseCssUrl()), array_keys($this->getLocalCssUrl()));
+        $buffer = [];
+
+        $baseCSS = $this->getBasePublicUrl('css');
+        $localCss = $this->getLocalPublicUrl('css');
+
+        $baseAssets = $this->getBaseAssets();
+        foreach($baseAssets as $k=>$v)
+        {
+            if( $v === 'css' )
+            {
+                $buffer[ $baseCSS . $k ]= $v;
+            }
+        }
+        $localAssets = $this->getLocalAssets();
+        foreach($localAssets as $k=>$v)
+        {
+            if( $v === 'css' )
+            {
+                $buffer[ $localCss . $k ]= $v;
+            }
+        }
+
+        return $buffer;
     }
 }
