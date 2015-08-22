00e8d4ac63c680e11b3aa08c4ebdae7bef3b10c1
inwebo <inwebo@gmail.com>
2015-08-22 20:23:17 +0200

    Module autoload

---
+++ Libre/Autoloader/BaseDir/ModuleBaseDir/ModuleBaseDir.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Libre\Autoloader\BaseDir {
+
+    use Libre\Autoloader\BaseDir;
+    use Libre\Autoloader\ClassInfos;
+
+    class ModuleBaseDir extends BaseDir
+    {
+
+        public function toPath()
+        {
+            $className = $this->_classInfos->trim();
+            $classObj = new ClassInfos($className);
+            $classArray = $classObj->toArray();
+            unset($classArray[0]);
+            unset($classArray[1]);
+            //var_dump($classArray);
+            $nameSpace = implode(DIRECTORY_SEPARATOR, $classArray);
+            $fileName = str_replace('{class}', $this->_classInfos->getClassName(), $this->_classFilePattern);
+            $path = $this->_baseDir . DIRECTORY_SEPARATOR . $nameSpace . DIRECTORY_SEPARATOR . $fileName;
+            $path = str_replace('_', DIRECTORY_SEPARATOR, $path);
+            return $path;
+        }
+
+    }
+}
