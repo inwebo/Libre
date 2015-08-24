12052255e4af982e9470a1d4ce0d03e253ae221d
inwebo <inwebo@gmail.com>
2015-08-24 07:56:30 +0200

    Add viewhelper

--- Libre/index.php
+++ Libre/index.php
@@ -1,11 +1,12 @@
 <?php
 namespace Libre {
     include_once 'Autoloader/autoload.php';
-
+    include_once 'viewhelpers.php';
     Autoloader\Handler::addDecorator(new Autoloader\BaseDir(__DIR__));
     spl_autoload_register( "\\Libre\\Autoloader\\Handler::handle" );
 
     define('CONFIG', DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'config.ini');
+    define('MVC_STATIC_CONTROLLER', '\\Libre\\Mvc\\Controller\\StaticController');
 
     class Modules{}
     class Admin{}
