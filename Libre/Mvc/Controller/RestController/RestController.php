990ae42390845ab4b20047ef88adafd326fed380
inwebo <inwebo@gmail.com>
2015-09-19 17:20:51 +0200

    Mass refactoring

--- Libre/Mvc/Controller/RestController/RestController.php
+++ Libre/Mvc/Controller/RestController/RestController.php
@@ -1,56 +1,131 @@
 <?php
 
-namespace Libre\Mvc\Controller\RestController;
+namespace Libre\Mvc\Controller\RestController {
 
-use Libre\Http\Authentification\IAuthenticable;
-use Libre\Mvc\Controller;
+    use Libre\Http\Authentification\IAuthenticable;
+    use Libre\Mvc\Controller;
 
-abstract class RestController extends Controller
-{
     /**
-     * @var array
+     * Class RestController
+     * @package Libre\Mvc\Controller\RestController
      */
-    protected $_decorators = array();
-
-    public function addDecorator(IAuthenticable $decorator)
+    abstract class RestController extends Controller
     {
-        $this->_decorators[] = $decorator;
-    }
+        #region Attributs
+        /**
+         * @var array
+         */
+        protected $_decorators = array();
 
-    /**
-     * @var bool
-     */
-    protected $_public = true;
+        /**
+         * @var bool
+         */
+        protected $_public = true;
+        /**
+         * @var array
+         */
+        protected $_verbsForceToPublic;
+        #endregion
 
-    /**
-     * @return boolean
-     */
-    public function isPublic()
-    {
-        return $this->_public;
-    }
-
-    /**
-     * @param boolean $public
-     */
-    public function setPublic($public)
-    {
-        $this->_public = $public;
-    }
-
-    /**
-     * Les classes héritées non publiques doivent ajouter les decorators AVANT d'appeller parent::_init
-     */
-    public function init()
-    {
-        parent::init();
-        $this->getResponse()->setForceRender(false);
-    }
-
-    public function indexAction(){
-        if( $this->validate() )
+        #region Getters / Setters
+        /**
+         * @param string $name
+         * @param IAuthenticable $decorator
+         */
+        protected function addDecorator($name, IAuthenticable $decorator)
         {
-            switch($this->getRequest()->getVerb()) {
+            $this->_decorators[$name] = $decorator;
+        }
+
+        /**
+         * @param string $name
+         * @return null|IAuthenticable
+         */
+        protected function getDecorator($name)
+        {
+            if( isset($this->_decorators[$name]) )
+            {
+                return $this->_decorators[$name];
+            }
+        }
+        /**
+         * @return array
+         */
+        protected function getDecorators()
+        {
+            return $this->_decorators;
+        }
+
+        /**
+         * @return boolean
+         */
+        public function isPublic()
+        {
+            return $this->_public;
+        }
+
+        /**
+         * @param boolean $public
+         */
+        protected function setPublic($public)
+        {
+            $this->_public = $public;
+        }
+
+        /**
+         * @return array
+         */
+        public function getVerbsForceToPublic()
+        {
+            return $this->_verbsForceToPublic;
+        }
+
+        /**
+         * @param string $verb
+         * @return string|bool
+         */
+        public function getVerbForceToPublic($verb)
+        {
+            if (isset($this->_verbsForceToPublic[strtolower($verb)])) {
+                return $this->_verbsForceToPublic[strtolower($verb)];
+            } else {
+                return false;
+            }
+        }
+
+        /**
+         * Peut être surchargée dans les classes filles pour un controle plus fin
+         */
+        protected function initVerbsForceToPublic()
+        {
+            $this->_verbsForceToPublic =
+                array(
+                    "get" => false,
+                    "post" => false,
+                    "options" => false,
+                    "update" => false,
+                    "delete" => false,
+                    "put" => false,
+                    "patch" => false
+                );
+        }
+        #endregion
+
+        /**
+         * Les classes héritées non publiques doivent ajouter les decorators AVANT d'appeller parent::_init
+         */
+        public function init()
+        {
+            parent::init();
+            if (!$this->validate()) {
+                $this->getResponse()->setStatusCode('HTTP/1.1 403 Forbidden');
+                return $this->getResponse();
+            }
+        }
+
+        public function indexAction()
+        {
+            switch ($this->getRequest()->getVerb()) {
                 case 'OPTIONS':
                     $this->options();
                     break;
@@ -81,114 +156,154 @@ abstract class RestController extends Controller
             }
         }
 
-    }
+        public function validate()
+        {
+            $isValid = false;
 
-    public function validate()
-    {
-        if( !$this->isPublic() )
-        {
-            if( !empty($this->_decorators) )
-            {
-                $valid = false;
-                /** @var IAuthenticable $decorator */
-                foreach($this->_decorators as $decorator)
-                {
-                    $valid &= $decorator->isValid();
+            if (!$this->isPublic()) {
+                if (!empty($this->_decorators)) {
+                    /** @var IAuthenticable $decorator */
+                    foreach ($this->_decorators as $decorator) {
+                        if ($decorator->isValid()) {
+                            $isValid = true;
+                            break;
+                        }
+                    }
                 }
-                if(!$valid)
-                {
-                    return false;
-                }
-            }
-            else
-            {
-                return false;
-            }
-        }
-        else
-        {
-            return true;
-        }
-    }
 
-    public function dispatch()
-    {
-        if( !$this->validate() )
+                $verb = $this->getRequest()->getVerb();
+                $isOverridingVisibility = $this->getVerbsForceToPublic($verb);
+
+                // Si aucun decorators ne renvoit vraie alors deniere chance de surcharge par la visibilité par verb
+                // Un controller privée peut tout de même rendre public certaine methodes
+                if (!$isValid) {
+                    $isValid = $isOverridingVisibility;
+                }
+
+            } else {
+                $isValid = true;
+            }
+
+            return $isValid;
+        }
+
+        public function dispatch()
         {
-            $this->getResponse()->setStatusCode('HTTP/1.1 403 Forbidden');
+            $this->negotiateContentType();
+            $method = $this->negotiateRenderMethod();
+            $this->prepareResponse($method);
             return $this->getResponse();
         }
-        switch($this->getRequest()->getHeader('Accept')) {
-            case 'application/json':
-                $this->getResponse()->setHeader('Accept', 'application/json');
-                $this->getResponse()->appendSegment('layout', $this->toJson());
-                break;
 
-            case 'text/xml':
-                $this->getResponse()->setHeader('Accept', 'text/xml');
-                $this->getResponse()->appendSegment('layout', $this->toXml());
-                break;
+        protected function negotiateContentType()
+        {
+            switch ($this->getRequest()->getHeader('Accept')) {
+                case 'application/json':
+                    $this->getResponse()->setHeader('Accept', 'application/json');
+                    break;
 
-            case 'text/plain':
-                $this->getResponse()->setHeader('Accept', 'text/plain');
-                $this->getResponse()->appendSegment('layout', $this->toText());
-                break;
+                case 'text/xml':
+                    $this->getResponse()->setHeader('Accept', 'text/xml');
+                    break;
 
-            default:
-            case 'text/html':
-                $this->getResponse()->setHeader('Accept', 'text/html');
-                $this->getResponse()->appendSegment('layout', $this->toHtml());
-                break;
+                case 'text/plain':
+                    $this->getResponse()->setHeader('Accept', 'text/plain');
+                    break;
+
+                default:
+                case 'text/html':
+                    $this->getResponse()->setHeader('Accept', 'text/html');
+                    break;
+            }
         }
-        return $this->getResponse();
+
+        protected function negotiateRenderMethod()
+        {
+            $verb = $this->getRequest()->getVerb();
+
+            switch ($this->getRequest()->getHeader('Accept')) {
+                case 'application/json':
+                    $to = 'toJson';
+                    break;
+
+                case 'text/xml':
+                    $to = 'toXml';
+                    break;
+
+                case 'text/plain':
+                    $to = 'toText';
+                    break;
+
+                default:
+                case 'text/html':
+                    $to = 'toHtml';
+                    break;
+            }
+
+            return $method = $verb . $to;
+        }
+
+        protected function prepareResponse($method)
+        {
+            if (method_exists($this, $method)) {
+                $method = new \ReflectionMethod($this, $method);
+                $this->getResponse()->appendSegment('layout', $method->invoke($this));
+            } else {
+                $this->getResponse()->appendSegment('layout', '');
+            }
+        }
+
+        #region Renderers
+        public function getToHtml()
+        {
+        }
+
+        public function getToJson()
+        {
+        }
+
+        public function getToXml()
+        {
+        }
+
+        public function getToText()
+        {
+        }
+        #endregion
+
+        #region Verbs
+        public function options()
+        {
+        }
+
+        public function get()
+        {
+        }
+
+        public function post()
+        {
+        }
+
+        public function head()
+        {
+        }
+
+        public function patch()
+        {
+        }
+
+        public function update()
+        {
+        }
+
+        public function delete()
+        {
+        }
+
+        public function put()
+        {
+        }
+        #endregion
+
     }
-
-
-
-    #region to
-    public function toHtml(){}
-    public function toJson(){}
-    public function toXml(){}
-    public function toText(){}
-    #endregion
-
-    #region Verbs
-    public function options()
-    {
-        echo __METHOD__;
-    }
-
-    public function get()
-    {
-        echo __METHOD__;
-    }
-
-    public function post()
-    {
-        echo __METHOD__;
-    }
-
-    public function head(){}
-
-    public function patch()
-    {
-        echo __METHOD__;
-    }
-
-    public function update()
-    {
-        echo __METHOD__;
-    }
-
-    public function delete()
-    {
-        echo __METHOD__;
-    }
-
-    public function put()
-    {
-        echo __METHOD__;
-    }
-    #endregion Verbs
-
 }
