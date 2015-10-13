4c6da0c042feec65844010682305508ab4c547e9
inwebo <inwebo@gmail.com>
2015-10-13 22:48:07 +0200

    Bug
    two /

--- Libre/Autoloader/BaseDir/ModuleBaseDir/ModuleBaseDir.php
+++ Libre/Autoloader/BaseDir/ModuleBaseDir/ModuleBaseDir.php
@@ -18,7 +18,7 @@ namespace Libre\Autoloader\BaseDir {
             //var_dump($classArray);
             $nameSpace = implode(DIRECTORY_SEPARATOR, $classArray);
             $fileName = str_replace('{class}', $this->_classInfos->getClassName(), $this->_classFilePattern);
-            $path = $this->_baseDir . DIRECTORY_SEPARATOR . $nameSpace . DIRECTORY_SEPARATOR . $fileName;
+            $path = $this->_baseDir .  $nameSpace . DIRECTORY_SEPARATOR . $fileName;
             $path = str_replace('_', DIRECTORY_SEPARATOR, $path);
             return $path;
         }
