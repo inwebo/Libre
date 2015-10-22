bf6cfea55b1e0727c45e36bbb123f7302bedb434
inwebo <inwebo@gmail.com>
2015-10-22 22:27:28 +0200

    throw RouterError404

--- Libre/System/Boot/BootStrap/Mvc/Task/Router/Router.php
+++ Libre/System/Boot/BootStrap/Mvc/Task/Router/Router.php
@@ -2,26 +2,51 @@
 
 namespace Libre\System\Boot\BootStrap\Mvc\Task {
 
+    use Libre\Exception\HttpException;
+    use Libre\Routing\Route;
     use Libre\Routing\Routed;
+    use Libre\Routing\RouterError404;
     use Libre\Routing\Uri;
     use Libre\System\Boot\BootStrap\Mvc\DefaultTask;
     use Libre\Routing\Router as _Router;
+    use Libre\System;
+    use Libre\Routing\RoutesCollection;
 
     class Router extends DefaultTask
     {
 
         protected function router()
         {
+
             try
             {
-                $router = new _Router(Uri::this(),$this->getSystem()->getRoutesCollection(),true);
+                // La collection default ne contient pas de route par defaut, false.
+                $router = new _Router(Uri::this(), $this->getSystem()->getRoutesCollection(), false);
                 $routed = $router->dispatch();
-                $this->getSystem()->setRouted($routed);
+                if(!$routed)
+                {
+                    throw new RouterError404();
+                }
+                else
+                {
+                    $this->getSystem()->setRouted($routed);
+                }
+
             }
             catch(\Exception $e)
             {
-                // @todo Default route
-                var_dump($e);
+                header('HTTP/1.1 404 Not Found');
+                /** @var Route $route */
+                if(RoutesCollection::get('__system')->count()>0)
+                {
+                    $route = RoutesCollection::get('__system')->getDefaultRoute();
+                    $routed= new Routed($route->getController(),$route->getAction(),array('exception'=>$e));
+                    $this->getSystem()->setRouted($routed);
+                }
+                else
+                {
+                    throw($e);
+                }
             }
         }
     }
