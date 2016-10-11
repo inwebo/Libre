1152d55fdea294e6663ca2906140bb8d7b0524db
inwebo <inwebo@gmail.com>
2016-10-11 15:23:59 +0200

    Add
    publicUrl();
    publicInstanceUrl();
    publicThemeUrl();

--- Libre/viewhelpers.php
+++ Libre/viewhelpers.php
@@ -38,3 +38,26 @@ function addRoute($route)
 {
     return System::this()->getRoutesCollection()->addRoute($route);
 }
+
+/**
+ * @return string Return global public dir url
+ */
+function publicUrl()
+{
+    return System::this()->getInstanceLocator()->getBaseUrl() . 'public/';
+};
+/**
+ * @return string Return instance public dir url
+ */
+function publicInstanceUrl()
+{
+    return System::this()->getInstance()->toUrl(). 'public/';
+};
+/**
+ * @param string Theme name
+ * @return string Return theme public dir url
+ */
+function publicThemeUrl($name)
+{
+    return System::this()->getTheme($name)->getPathsLocator()->getBaseUrl() . 'public/';
+};
