c24779551eccf6985c35c0c4d298446a7186c87c
inwebo <inwebo@gmail.com>
2016-11-11 16:01:55 +0100

    Step demo

--- demos/menu.php
+++ demos/menu.php
@@ -8,17 +8,41 @@ namespace Libre {
 
     try {
 
-        $menu = new Menu('main',null,null);
+        $menu = new Menu(0,"main","http://www.test.fr");
 
-        $menu->addItem('Bonjour','/admin',1);
-        $menu->addItem('Le monde', 'http://www.inwebo.dev', 0);
+        $menu->addItem('Bonjour', '/admin', 0);
+        $menu->addItem('Le monde', '/test', 1);
+        $menu->addItem('Le 5', '/arf', 5);
+        $menu->addItem('Le 6', '/arf', 6);
+        $menu->addItem('Le 7', '/arf', 7);
+        $menu->addItem('Le 8', '/arf', 8);
 
-        var_dump($menu->getQueue()->count());
-        $generator = $menu->generator();
-        foreach($generator as $g)
+        $menu2 = new Menu(1,"arf","http://www.2.test.fr");
+        $menu2->addItem('Bonjour', '/admin', 0);
+        $menu2->addItem('Le monde', '/test', 1);
+        $menu2->addItem('Le 3', '/arf', 2);
+
+        $menu->addMenu($menu2,4);
+
+        function displayMenu(Menu\IMenu $menu)
         {
-            echo '<a href="'.$g->getUri().'">'.$g->getLabel().'</a><br>';
+            $buffer = "<ul>";
+            $generator = $menu->generator();
+            foreach($generator as $g)
+            {
+                if($g instanceof Menu){
+                    $buffer .= \Libre\displayMenu($g);
+                }
+                else {
+                    $buffer .= '<li><a href="'.$menu->getBaseUrl() . $g->getUri().'">'.$g->getLabel().'</a><br></li>';
+                }
+            }
+            $buffer .= "</ul>";
+            return $buffer;
         }
+
+        echo \Libre\displayMenu($menu);
+
     } catch (\Exception $e) {
         echo $e->getMessage();
     }
