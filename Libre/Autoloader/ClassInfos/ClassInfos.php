9a59c76f1fe074fb6a56ca23ca464a16e310d221
inwebo <julien@francepronet.com>
2017-11-05 10:56:11 +0100

    Refacto 2018

--- Libre/Autoloader/ClassInfos/ClassInfos.php
+++ Libre/Autoloader/ClassInfos/ClassInfos.php
@@ -1,131 +1,156 @@
 <?php
+/**
+ *
+ */
+namespace Libre\Autoloader;
 
-namespace Libre\Autoloader {
+/**
+ * Class CoreClass
+ */
+class ClassInfos
+{
+    /**
+     * @var string
+     */
+    protected $class;
+    /**
+     * @var string
+     */
+    protected $extension = '.php';
 
     /**
-     * Class CoreClass
-     *
-     * @package Libre\Autoloader
+     * @return string
      */
-    class ClassInfos
+    public function getClass()
     {
-        /**
-         * @var string
-         */
-        protected $_class;
-        /**
-         * @var string
-         */
-        protected $_extension = '.php';
-
-        /**
-         * @return string
-         */
-        public function getClass()
-        {
-            return $this->_class;
-        }
-
-        /**
-         * @param string $class
-         */
-        protected function setClass($class)
-        {
-            $this->_class = $class;
-        }
-
-        /**
-         * @return string
-         */
-        public function getExtension()
-        {
-            return $this->_extension;
-        }
-
-        /**
-         * @param string $extension
-         */
-        public function setExtension($extension)
-        {
-            $this->_extension = $extension;
-        }
-
-        /**
-         * @param string $class Class name
-         */
-        public function __construct($class)
-        {
-            $this->setClass($class);
-        }
-
-        public function trim()
-        {
-            return trim($this->getClass(), '\\');
-        }
-
-        public function isNamespaced()
-        {
-            return (strpos($this->getClass(), '\\') !== false) ? true : false;
-        }
-
-        /**
-         * @param int $offset
-         * @return null|string
-         */
-        public function getVendor($offset = 1)
-        {
-            if ($this->isNamespaced()) {
-                $asArray = explode('\\', $this->trim());
-                if ($offset > 1) {
-                    $a = $asArray;
-                    $toPop = count($a) - $offset;
-                    for ($i = 0; $i < $toPop; $i++) {
-                        array_pop($a);
-                    }
-                    return implode('\\', $a);
-                } else {
-                    return (isset($asArray[0]) && !empty($asArray[0])) ? $asArray[0] : $this->getClass();
-                }
-
-            } else {
-                return null;
-            }
-        }
-
-        public function getClassName()
-        {
-            $v = $this->toArray();
-            return end($v);
-        }
-
-        public function toAbsolute()
-        {
-            return '\\' . $this->trim();
-        }
-
-        public function toArray()
-        {
-            $v = array();
-            if ($this->isNamespaced()) {
-                $array = explode('\\', $this->trim());
-                $v = $array;
-            } else {
-                $v[] = $this->getClass();
-            }
-            return $v;
-        }
-
-        public function toPSR0($baseDir)
-        {
-            $str = str_replace(array('\\', '_'), DIRECTORY_SEPARATOR, $this->getClass());
-            return $baseDir . $str . $this->getExtension();
-        }
-
-        public function isLoaded()
-        {
-            return class_exists($this->getClass(), false);
-        }
-
+        return $this->class;
     }
 
+    /**
+     * @return string
+     */
+    public function getExtension()
+    {
+        return $this->extension;
+    }
+
+    /**
+     * @param string $extension
+     */
+    public function setExtension($extension)
+    {
+        $this->extension = $extension;
+    }
+
+    /**
+     * @param string $class Class name
+     */
+    public function __construct($class)
+    {
+        $this->setClass($class);
+    }
+
+    /**
+     * @return string
+     */
+    public function trim()
+    {
+        return trim($this->getClass(), '\\');
+    }
+
+    /**
+     * @return bool
+     */
+    public function isNamespaced()
+    {
+        return (strpos($this->getClass(), '\\') !== false) ? true : false;
+    }
+
+    /**
+     * @param int $offset
+     *
+     * @return null|string
+     */
+    public function getVendor($offset = 1)
+    {
+        if ($this->isNamespaced()) {
+            $asArray = explode('\\', $this->trim());
+            if ($offset > 1) {
+                $a = $asArray;
+                $toPop = count($a) - $offset;
+                for ($i = 0; $i < $toPop; $i++) {
+                    array_pop($a);
+                }
+
+                return implode('\\', $a);
+            } else {
+                return (isset($asArray[0]) && !empty($asArray[0])) ? $asArray[0] : $this->getClass();
+            }
+
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getClassName()
+    {
+        $array = $this->toArray();
+
+        return end($array);
+    }
+
+    /**
+     * @return string
+     */
+    public function toAbsolute()
+    {
+        return '\\'.$this->trim();
+    }
+
+    /**
+     * @return array
+     */
+    public function toArray()
+    {
+        $v = [];
+        if ($this->isNamespaced()) {
+            $array = explode('\\', $this->trim());
+            $v = $array;
+        } else {
+            $v[] = $this->getClass();
+        }
+
+        return $v;
+    }
+
+    /**
+     * @param string $baseDir
+     *
+     * @return string
+     */
+    public function toPSR0(string $baseDir)
+    {
+        $str = str_replace(['\\', '_'], DIRECTORY_SEPARATOR, $this->getClass());
+
+        return $baseDir.$str.$this->getExtension();
+    }
+
+    /**
+     * @return bool
+     */
+    public function isLoaded()
+    {
+        return class_exists($this->getClass(), false);
+    }
+
+    /**
+     * @param string $class
+     */
+    protected function setClass($class)
+    {
+        $this->class = $class;
+    }
 }
--- demos/class-infos.php
+++ demos/class-infos.php
@@ -1,12 +1,13 @@
 <?php
-namespace Libre{
+
+namespace Libre {
 
     use Libre\Autoloader\ClassInfos;
-
+    use Libre\Http\Authentification\ByPublicKey;
     include_once 'header.php';
 
-    try{
-        $classInfos = new ClassInfos('\\test\\arf\\no\\Wat_the_Duck');
+    try {
+        $classInfos = new ClassInfos(ByPublicKey::class);
         var_dump($classInfos->getClass());
         var_dump($classInfos->getVendor(2));
         var_dump($classInfos->getClassName());
@@ -14,11 +15,9 @@ namespace Libre{
         var_dump($classInfos->toAbsolute());
         var_dump($classInfos->toArray());
         var_dump($classInfos->trim());
-        var_dump($classInfos->toPSR0('../Libre'));
+        var_dump($classInfos->toPSR0('../'));
         var_dump($classInfos->isLoaded());
-    }
-    catch(\Exception $e)
-    {
+    } catch (\Exception $e) {
         echo $e->getMessage();
     }
 }
