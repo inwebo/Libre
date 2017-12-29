21cc704ba51fa000a39b67ce367e8544d31bb381
julien <julien@francepronet.com>
2017-12-29 16:08:40 +0100

    PSR

---
+++ Libre/Helpers/Benchmark/BenchmarkCallBackException.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @author    Julien Hannotin <julien@ddf.agency>
+ *
+ * @copyright 2017 Digital Dealer Factory
+ *
+ * @license   All rights reserved
+ *
+ * Date: 28/12/17
+ * Time: 16:01
+ *
+ */
+
+namespace Libre\Helpers\Benchmark;
+
+/**
+ * @author    Julien Hannotin <julien@ddf.agency>
+ *
+ * @copyright 2017 Digital Dealer Factory
+ *
+ * @license   All rights reserved
+ *
+ * Date: 28/12/17
+ * Time: 16:01
+ *
+ */
+class BenchmarkCallBackException extends \Exception
+{
+
+}
